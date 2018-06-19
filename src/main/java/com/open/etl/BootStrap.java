dce1e9ebb8713fa5372d02e413ade2103155a4b7
yinshixiao <593853565@qq.com>
2018-06-19 23:30:52 +0800

    1

--- src/main/java/com/open/etl/BootStrap.java
+++ src/main/java/com/open/etl/BootStrap.java
@@ -3,7 +3,7 @@ package com.open.etl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class BootStrap {
 
 	public static void main(String[] args) {
