cb990697ef7c5c1c034f610f27396b62b6a88884
yinshixiao <593853565@qq.com>
2018-06-19 23:54:45 +0800

    1

--- src/main/java/com/open/etl/MsToMySql/TestController.java
+++
@@ -1,9 +0,0 @@
-package com.open.etl.MsToMySql;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TestController {
-
-	
-}
---
+++ src/main/java/com/open/etl/MsToMySql/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.open.etl.MsToMySql.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping
+	public Object getSqlServerInfo() {
+		
+		return null;
+	}
+}
